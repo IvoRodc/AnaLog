cff3caf9328d5dfb539823ba22b5a3eda7f830f7
Marcos Gaspar <Marga5@users.noreply.github.com>
2018-11-24 16:08:16 +0000

    Package DB - PARTE 3

--- app/src/main/java/pt/isec/gps/grupo14/analog/Classes/DBInterface.java
+++ app/src/main/java/pt/isec/gps/grupo14/analog/Classes/DBInterface.java
@@ -1,15 +1,22 @@
 package pt.isec.gps.grupo14.analog.Classes;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.nfc.NfcAdapter;
+import android.provider.ContactsContract;
 import android.util.Log;
 
 import java.util.Date;
 
+import pt.isec.gps.grupo14.analog.ExposicaoActivity;
+
 public class DBInterface extends SQLiteOpenHelper {
+
+
+    //Nome da base de dados
+    private static final String database_name = "AnaLog";
 
     //region TABELA CAMERA
     private static final String table_camera = "Camera";
@@ -48,11 +55,15 @@ public class DBInterface extends SQLiteOpenHelper {
     //endregion
 
 
+    public DBInterface(Context context){
+        super(context, database_name, null, 0);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
         //region CREATE_TABLE_CAMERA
-        String CREATE_TABLE_CAMERA = "CREATE TABLE IF NOT EXISTS"
+        String CREATE_TABLE_CAMERA = "CREATE TABLE IF NOT EXISTS "
                 + table_camera + " ("
                 + IDcam + "INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + MarcaCam +  "VARCHAR(20), "
@@ -60,7 +71,7 @@ public class DBInterface extends SQLiteOpenHelper {
         //endregion
 
         //region CREATE_TABLE_OBJETIVA
-        String CREATE_TABLE_OBJETIVA = "CREATE TABLE IF NOT EXISTS"
+        String CREATE_TABLE_OBJETIVA = "CREATE TABLE IF NOT EXISTS "
                 + table_objetiva + " ("
                 + IDobj + "INTEGER,"
                 + MarcaObj + " VARCHAR(20),"
@@ -68,7 +79,7 @@ public class DBInterface extends SQLiteOpenHelper {
         //endregion
 
         //region CREATE_TABLE_ROLO
-        String CREATE_TABLE_ROLO = "CREATE TABLE IF NOT EXISTS"
+        String CREATE_TABLE_ROLO = "CREATE TABLE IF NOT EXISTS "
         + table_rolo + " ("
         + IDrolo + "INTEGER PRIMARY KEY AUTOINCREMENT, "
         + Titulo + " VARCHAR(20),"
@@ -81,9 +92,8 @@ public class DBInterface extends SQLiteOpenHelper {
         //endregion
 
 
-
         //region CREATE_TABLE_EXPOSICAO
-        String CREATE_TABLE_EXPOSICAO = "CREATE TABLE IF NOT EXISTS"
+        String CREATE_TABLE_EXPOSICAO = "CREATE TABLE IF NOT EXISTS "
                 + table_exposicao + " ("
                 + IDexp + " INTEGER, "
                 + VelDisparo + " INTEGER,"
@@ -103,10 +113,6 @@ public class DBInterface extends SQLiteOpenHelper {
         db.execSQL(CREATE_TABLE_ROLO);
         db.execSQL(CREATE_TABLE_EXPOSICAO);
 
-
-
-
-
     }
 
     @Override
@@ -115,24 +121,24 @@ public class DBInterface extends SQLiteOpenHelper {
         //onCreate(db);
     }
 
-    public int addRolo (Rolo r){
+    public int addRolo (Rolo rolo){
 
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues value = new ContentValues();
-        value.put(IDrolo, r.getID());
-        value.put(Titulo, r.getTitulo());
-        value.put(ISO, r.getISO());
-        value.put(Formato, r.getFormato());
-        value.put(NExposicoes, r.getMaxExposicoes());
-        value.put(DescricaoRolo, r.getDescricao());
-        value.put(Revelado, r.getRevelado());
-        value.put(DataRolo, r.getDate());
+        value.put(IDrolo, rolo.getID());
+        value.put(Titulo, rolo.getTitulo());
+        value.put(ISO, rolo.getISO());
+        value.put(Formato, rolo.getFormato());
+        value.put(NExposicoes, rolo.getMaxExposicoes());
+        value.put(DescricaoRolo, rolo.getDescricao());
+        value.put(Revelado, rolo.getRevelado());
+        value.put(DataRolo, rolo.getDate());
 
         //Insert Row
         db.insert(table_rolo, null, value);
 
-        String selectQuery = "SELECT " + IDrolo + " FROM " + table_rolo + " ORDER BY DESC";
+        String selectQuery = "SELECT " + IDrolo + " FROM " + table_rolo + " ORDER BY 'DESC' ";
         Cursor cursor = db.rawQuery(selectQuery, null);
 
         db.close();
@@ -140,14 +146,79 @@ public class DBInterface extends SQLiteOpenHelper {
         return cursor.getInt(0);
     }
 
-    public void dropRolos(){
+    public void removeRolo(){
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("DELETE FROM " + table_rolo);
         db.close();
     }
 
+    public int addExposicao(int IDRolo, Exposicao exp){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues value = new ContentValues();
+
+        value.put(IDexp, exp.getID());
+        value.put(VelDisparo, exp.getVelDisparo());
+        value.put(Abertura, exp.getAbertura());
+        value.put(DistFocal, exp.getDistFocal());
+        value.put(DescricaoExp, exp.getDescricao());
+        value.put(DataExp, exp.getData());
+
+        //Insert Row
+        db.insert(table_exposicao,null,value);
+
+        String selectQuery = "SELECT " + IDexp + " FROM " + table_exposicao + " ORDER BY 'DESC' ";
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        db.close();
+
+        return cursor.getInt(0);
+    }
+
+    public void removeExposicao(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + table_exposicao);
+        db.close();
+    }
 
 
+    public void addCamera(Camera cam) {
+        SQLiteDatabase db = this.getWritableDatabase();
 
+        ContentValues value = new ContentValues();
 
+        value.put(IDcam, cam.getID());
+        value.put(MarcaCam, cam.getMarca());
+        value.put(ModeloCam, cam.getModelo());
+
+        //Insert Row
+        db.insert(table_camera, null, value);
+        db.close();
+    }
+
+    public void removeCamera(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + table_camera);
+        db.close();
+    }
+
+    public void addObjetiva (Objetiva obj){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues value = new ContentValues();
+
+        value.put(IDobj, obj.getID());
+        value.put(MarcaObj, obj.getMarca());
+        value.put(ModeloObj, obj.getModelo());
+
+        //Insert Row
+        db.insert(table_objetiva, null, value);
+        db.close();
+    }
+
+    public void removeObjetiva(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + table_objetiva);
+        db.close();
+    }
 }
