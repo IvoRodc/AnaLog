b8809dc09e10472ba76f3325ed449e6b0b1bd7c5
Ivo Rodrigues <14186480+IvoRodc@users.noreply.github.com>
2018-11-27 17:41:01 +0000

    Formulário para adicionar rolo

---
+++ app/src/main/java/pt/isec/gps/grupo14/analog/BottomSheet/BottomSheet_AddRolo.java
@@ -0,0 +1,53 @@
+package pt.isec.gps.grupo14.analog.BottomSheet;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import pt.isec.gps.grupo14.analog.R;
+
+public class BottomSheet_AddRolo extends BottomSheetDialogFragment {
+
+    private boolean open;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.bottom_sheet_add_rolo, container, false);
+        return v;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new BottomSheetDialog(getContext(), getTheme());
+    }
+
+    @Override
+    public int getTheme() {
+        return R.style.BaseBottomSheetDialog;
+    }
+
+    @Override
+    public void show(FragmentManager manager, String tag) {
+        super.show(manager, tag);
+        open=true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        open=false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+}
